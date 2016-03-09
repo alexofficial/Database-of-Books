bb0e3b7d82693c2053b5468b0a06399d7e8d3b27
Alexander Patsanis <>
2016-03-09 16:53:40 +0200

    Beta Started  Version 1.0

---
+++ application/views/body_content_2/home_page_content.php
@@ -0,0 +1,35 @@
+<body>
+
+<div class="container-fluid">
+  <div class="row content">
+    <div class="col-sm-3 sidenav">
+      <h4>Βάση Βιβλίων</h4>
+      <ul class="nav nav-pills nav-stacked">
+        <li class="active"><a href="#section1">Αρχική</a></li>
+        <li><a href="#section2">Βιβλία</a></li>
+        <li><a href="#section3">Συγγραφείς</a></li>
+        
+      </ul><br>
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Αναζήτηση">
+        <span class="input-group-btn">
+          <button class="btn btn-default" type="button">
+            <span class="glyphicon glyphicon-search"></span>
+          </button>
+        </span>
+      </div>
+    </div>
+
+    <div class="col-sm-9">
+      <h4><small>Λίστα Βιβλίων</small></h4>
+     
+    </div>
+  </div>
+</div>
+
+<footer class="container-fluid">
+  <p>Footer Text</p>
+</footer>
+
+</body>
+</html>
